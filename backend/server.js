52a24985024ab9844af9b872a8ff8c491cca3755
Sunny Larosa <sunnylarosa121@gmail.com>
2022-11-20 16:21:45 +0700

    Create DB connection to local database

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 // Import
 const express = require("express");
 const workoutRoutes = require("./routes/workout");
+const sql = require("mssql/msnodesqlv8");
 
 // Declare
 const app = express();
@@ -23,8 +24,35 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 
 // Connect to db
+const sqlConfig = {
+  database: "SASKI_Dev",
+  server: "DESKTOP-L4KGN47",
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: true,
+  },
+};
+
+sql
+  .connect(sqlConfig)
+  .then(() => {
+    console.log("DB connect successfully");
+
+    try {
+      // const result =
+      //   await sql.query`select * from [user] where UserID = 'user001'`;
+      // console.dir(result);
+
+      app.listen(process.env.PORT, () => {
+        console.log(`Running on port ${process.env.PORT}`);
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  })
+  .catch((err) => console.log(`Failed to connect with error: ${err}`));
 
 // Listen for requests
-app.listen(process.env.PORT, () => {
-  console.log(`Running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Running on port ${process.env.PORT}`);
+// });
