b07e3b286ce4cbc7e1b2b44dec4414b886c89fe4
Sunny Larosa <sunnylarosa121@gmail.com>
2022-11-21 02:00:54 +0700

    Create POST a new workout

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,6 @@
 // Import
 const express = require("express");
+const Workout = require("../models/workoutModel");
 
 // Declare the instance of route so it can be used in this file
 const router = express.Router();
@@ -15,8 +16,21 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post("/", (req, res) => {
-  res.json({ message: "POST a new workout" });
+router.post("/", async (req, res) => {
+  // Grab the properties from request body
+  const { title, reps, load } = req.body;
+
+  // Add a new document to the workout collection
+  try {
+    // Create a new workout document
+    const workout = await Workout.create({ title, reps, load });
+
+    // Send a response
+    res.status(200).json(workout);
+  } catch (error) {
+    // Return the response that contains error message
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // DELETE a workout
