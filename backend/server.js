5764b573196845190a0c02a9d5639a0f83a3bdc3
Sunny Larosa <sunnylarosa121@gmail.com>
2022-11-20 18:40:32 +0700

    Delete unnecessary comment

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,8 @@ require("dotenv").config();
 // Import
 const express = require("express");
 const workoutRoutes = require("./routes/workout");
-const sql = require("mssql/msnodesqlv8");
-<<<<<<< HEAD
-
 const mongoose = require("mongoose");
-=======
->>>>>>> 52a24985024ab9844af9b872a8ff8c491cca3755
+
 
 // Declare
 const app = express();
@@ -27,36 +23,6 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/workouts", workoutRoutes);
-
-// Connect to db
-// const sqlConfig = {
-//   database: "SASKI_Dev",
-//   server: "DESKTOP-L4KGN47",
-//   driver: "msnodesqlv8",
-//   options: {
-//     trustedConnection: true,
-//   },
-// };
-
-// sql
-//   .connect(sqlConfig)
-//   .then(() => {
-//     console.log("DB connect successfully");
-
-//     try {
-//       // const result =
-//       //   await sql.query`select * from [user] where UserID = 'user001'`;
-//       // console.dir(result);
-
-//       // Listen for requests
-//       app.listen(process.env.PORT, () => {
-//         console.log(`Running on port ${process.env.PORT}`);
-//       });
-//     } catch (e) {
-//       console.log(e.message);
-//     }
-//   })
-//   .catch((err) => console.log(`Failed to connect with error: ${err}`));
 
 // Connect to db (MongoDB)
 mongoose
