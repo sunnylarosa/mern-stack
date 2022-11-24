e28c72c79690532a2786f02e4581d2130b031283
Sunny Larosa <sunnylarosa121@gmail.com>
2022-11-24 08:41:43 +0700

    Import function from 'react-router-dom' package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,9 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// BroserRouter: wraps everywhere we want to use router
+// Routes: wraps all of our individual routes
+// Route: the individual route component to create a single route
+
 function App() {
   return <div className="App"></div>;
 }
