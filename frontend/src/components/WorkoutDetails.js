e7b35d63db6aa788729d217a14016f21f165b474
Sunny Larosa <sunnylarosa121@gmail.com>
2022-11-25 16:44:51 +0700

    Create WorkoutDetails component

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,21 @@
+// Destructure from the props
+const WorkoutDetails = ({ workout }) => {
+  // Return a template
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
+
+// Export
+export default WorkoutDetails;
