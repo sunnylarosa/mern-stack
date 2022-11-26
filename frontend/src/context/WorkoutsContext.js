0962e016fdb09216160c13f07cb17ddb80ebeb89
Sunny Larosa <sunnylarosa121@gmail.com>
2022-11-26 11:42:30 +0700

    Declare useReducer hook

--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -6,7 +6,7 @@
 
 // Import
 // 1. createContext -> let us create a new context that we can then provide to components in our application.
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 // Create the context
 export const WorkoutsContext = createContext();
@@ -14,6 +14,11 @@ export const WorkoutsContext = createContext();
 // Provide the context to our application component tree so that our components can access it.
 //   - { children } property represents whatever components or template that the 'WorkoutsContextProvider' context wraps. In this case, then the children is the App component.
 export const WorkoutsContextProvider = ({ children }) => {
+  // Declare useReducer
+  const [state, dispatch] = useReducer(workoutsReducer, { workouts: null });
+
+  // dispatch() function should have 'type' properties to describes the state change, and a 'payload' prop to represents any data we need to make the change.
+
   // Return a template, which is our workout context (WorkoutsContext) + .Provider
   return (
     // Whatever parts in this wrap will have access to the context. In this case, we need to wrap the whole application.
